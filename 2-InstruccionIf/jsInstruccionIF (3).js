ad994e014250bb6644aba7b4f144d2cb33aaae0e
Alansllano46 <52495010+Alansllano46@users.noreply.github.com>
2019-07-10 12:24:23 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,18 @@
 function mostrar()
+{//tomo la edad  
+    
+    var edad;
+
+    edad =parseInt(document.getElementById("edad").value);
+
+    if (edad >= 18) { 
+        
+        alert ("Es mayor de edad"); 
+
+
+}
+else
 {
-//tomo la edad  
-
-
-}//FIN DE LA FUNCIÓN
+     alert ("Es menor de edad");
+}
+}
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,28 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad;
+
+edad =parseInt(document.getElementById("edad").value);
 
 
+    
+if (edad >= 18) { 
+    alert ("Usted es adulto");
+}
 
-}//FIN DE LA FUNCIÓN
+else
+
+if (edad >=13 && edad <=17)
+{
+alert ("Usted es adolecente");}
+
+
+else
+
+if (edad <13){
+
+    alert ("Usted es un niño");
+}
+}
+//FIN DE LA FUNCIÓN
