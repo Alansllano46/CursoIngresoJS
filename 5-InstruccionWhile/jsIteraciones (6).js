f5030e5b6678bb7ae53acd661ed95ca706f88383
Alansllano46 <52495010+Alansllano46@users.noreply.github.com>
2019-07-11 12:33:25 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,25 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+	var numero;
+	var suma=0;
+	var promedio;
+	var contador = 0;
+	
+	while( contador < 5) {
+
+	numero = parseInt(prompt("ingrese un numero:"));
+	suma += numero;
+
+	contador++;
+}
+
+promedio = suma / 5;
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+
+document.getElementById('suma').value=suma;
+document.getElementById('promedio').value=promedio/5;
 
 }//FIN DE LA FUNCIÓN
