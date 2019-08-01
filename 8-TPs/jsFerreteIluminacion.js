51a0c0cbf4b6c34b82a230d6d6c229e15242f101
Alansllano46 <52495010+Alansllano46@users.noreply.github.com>
2019-08-01 12:23:13 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,79 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    var cantidad;
+    var precio = 35;
+    var precioConDescuento;
+    var porcDescuento;
+    var marca;
+    var IIBB;
+    var importeFinal;
+    var flag = 0;
+
+    cantidad = parseInt(document.getElementById("Cantidad").value);
+    marca = document.getElementById("Marca").value;
+
+    switch (cantidad) {
+        case 1:
+        case 2:
+            porcDescuento = 0;
+            break;
+        case 3:
+            if (marca == "ArgentinaLuz") {
+                porcDescuento = 15;
+            }
+            else if (marca == "FelipeLamparas") {
+                porcDescuento = 10;
+            }
+            else {
+                porcDescuento = 5;
+            }
+            break;
+
+        case 4:
+            if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+                porcDescuento = 25;
+            }
+            else {
+                porcDescuento = 20;
+            }
+            break;
+
+        case 5:
+            if (marca == "ArgentinaLuz") {
+                porcDescuento = 40;
+            }
+            else {
+                porcDescuento = 30;
+            }
+            break;
+
+        default:
+            porcDescuento = 50;
+
+    }
+    precioConDescuento = precio - porcDescuento / 100;
+
+    importeFinal = cantidad * precioConDescuento;
+
+    if (importeFinal > 120) {
+        IIBB = importeFinal * .10;
+        importeFinal = importeFinal + IIBB;
+        flag = 1;
+    }
+
+    alert("Importe Final $ " + importeFinal);
+
+    if (flag == 1) {
+        alert("Usted pago de IIBB $ " + IIBB);
+    }
+
+
+    
+      
+
+
+
+    
+
 }
